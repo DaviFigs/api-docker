273ce506514c8767bd712e19b4d88a0cd2c65350
Davi Figueiredo <devdeoliveira06@gmail.com>
2024-01-11 22:25:18 -0400

    --relaod

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def home():
-    return {'test':'success'}
+@app.get('/add_task')
+def add_task(name_task:str,importance:int):
+    pass
